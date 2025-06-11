b6ddde9f6930bcf7cdbbcef2ab23f72b4c8b5352
NgocHoang <tth251294@gmail.com>
2025-06-11 15:30:23 +0900

    html file updated

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -11,7 +11,13 @@ Office.onReady((info) => {
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
   }
+  let btn = document.querySelector("#btn")
+  btn.addEventListener('click', () => {
+    console.log("ddd");
+    
+  })
 });
+
 
 export async function run() {
   /**
@@ -19,10 +25,6 @@ export async function run() {
    */
 
   const item = Office.context.mailbox.item;
-  let insertAt = document.getElementById("item-subject");
-  let label = document.createElement("b").appendChild(document.createTextNode("Subject: "));
-  insertAt.appendChild(label);
-  insertAt.appendChild(document.createElement("br"));
-  insertAt.appendChild(document.createTextNode(item.subject));
-  insertAt.appendChild(document.createElement("br"));
+  console.log("jello");
+  
 }
