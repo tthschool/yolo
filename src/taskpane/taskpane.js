a1256f20e04693c3e29a919dce3066b1d4a32c36
trinhthaihoang <131327090+tthschool@users.noreply.github.com>
2025-06-11 16:09:15 +0900

    Update taskpane.js

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,25 +1,19 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
 /* global document, Office */
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
-    console.log("dajskndlasjkndlkasndlk")
+    console.log("Office.js is ready in Outlook");
+
     const btn = document.querySelector("#btn");
-    btn.addEventListener("click", () => run() )}
+    if (btn) {
+      btn.addEventListener("click", () => run());
+    } else {
+      console.warn("Button not found!");
+    }
+  }
 });
 
-
 async function run() {
-  /**
-   * Insert your Outlook code here
-   */
-
   const item = Office.context.mailbox.item;
   console.log("jello");
-  
 }
-    console.log("dajskndlasjkndlkasndlk")
